409328265db7889ebb3d5e7078da015448527a6e
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-24 08:30:11 -0600

    Complete user story 1

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'flights index page' do
+  before(:each) do
+    @airline = Airline.create!(name: "United")
+    @flight1 = @airline.flights.create!(number: 458, date: "02/12/2021", departure_city: "Denver", arrival_city: "Little Rock")
+    @flight2 = @airline.flights.create!(number: 1445, date: "08/14/2021", departure_city: "Anchorage", arrival_city: "Seattle")
+    @passenger1 = @flight1.passengers.create!(name: "John", age: 56)
+    @passenger2 = @flight1.passengers.create!(name: "Sarah", age: 27)
+    @passenger3 = @flight2.passengers.create!(name: "Jessica", age: 30)
+    @passenger4 = @flight2.passengers.create!(name: "Tony", age: 20)
+
+    visit flights_path
+  end
+
+  it 'displays all flight numbers' do
+    expect(page).to have_content(@flight1.number)
+    expect(page).to have_content(@flight2.number)
+  end
+
+  it 'displays each airline name' do
+    expect(page).to have_content(@airline.name)
+  end
+
+  it 'displays all of the flights passengers' do
+    expect(page).to have_content(@passenger1.name)
+    expect(page).to have_content(@passenger2.name)
+    expect(page).to have_content(@passenger3.name)
+    expect(page).to have_content(@passenger4.name)
+  end 
+end
